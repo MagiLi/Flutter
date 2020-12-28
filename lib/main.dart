ff0c50272106925f4297b8a136b5f7f046ca8374
lichaoqun <976906595@qq.com>
2020-12-28 15:54:14 +0800

    添加一个Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -5,17 +5,31 @@ void main() => runApp(new MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
+
     return new MaterialApp(
       title: 'Welcome to Flutter 1',
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Welcome to Flutter 2'),
+          title: new Text('Welcome to Flutter'),
         ),
         body: new Center(
-          child: new Text(wordPair.asCamelCase),
+          child: new RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    final wordPari = new WordPair.random();
+    return new Text(wordPari.asPascalCase);
   }
 }
