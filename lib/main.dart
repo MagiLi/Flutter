790ab5c141a86ca746173937023d90a07258483f
lichaoqun <976906595@qq.com>
2020-12-30 14:51:40 +0800

    导航到新页面

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return new MaterialApp(
       title: 'Welcome to Flutter 1',
       home: new RandomWords(),
@@ -19,23 +19,21 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
-
   final _suggestions = <WordPair>[];
-  final _saved = new Set<WordPair>();//点赞，喜欢的内容
+  final _saved = new Set<WordPair>(); //点赞，喜欢的内容
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   Widget _buildSuggestions() {
     return new ListView.builder(
       padding: const EdgeInsets.all(15.0),
       itemBuilder: (context, i) {
-        if (i.isOdd) return new Divider();//分割线
-        final index = i ~/ 2;//向下取整，可以计算出除去分割线后的实际数量
+        if (i.isOdd) return new Divider(); //分割线
+        final index = i ~/ 2; //向下取整，可以计算出除去分割线后的实际数量
         if (index >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
-        } else {
-        }
+        } else {}
         return _buildRow(_suggestions[index]);
-      }
+      },
     );
   }
 
@@ -48,7 +46,7 @@ class RandomWordsState extends State<RandomWords> {
       ),
       trailing: new Icon(
         alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
+        color: alreadySaved ? Colors.orange : null,
       ),
       onTap: () {
         setState(() {
@@ -62,11 +60,50 @@ class RandomWordsState extends State<RandomWords> {
     );
   }
 
+  void _pushSaved() {
+    Navigator.of(context).push(
+      new MaterialPageRoute(
+        builder: (context) {
+          final tiles = _saved.map(
+            (pair) {
+              return new ListTile(
+                title: new Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return new Scaffold(
+            appBar: new AppBar(
+              title: new Text("saved suggestion"),
+            ),
+            body: new ListView(
+              padding: new EdgeInsets.all(15.0),
+              children: divided,
+            ),
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text("Startup Name Generator"),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.list),
+            onPressed: _pushSaved,
+          ),
+        ],
       ),
       body: _buildSuggestions(),
     );
